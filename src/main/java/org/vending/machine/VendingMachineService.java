c7e19989dcaaf32fd02e8efeff7aa2b5f02d062f
amanissery <anoop.apma@gmail.com>
2018-02-13 09:40:11 +1100

    Unit test for valid and invalid denominations of currency

--- src/main/java/org/vending/machine/VendingMachineService.java
+++ src/main/java/org/vending/machine/VendingMachineService.java
@@ -2,6 +2,26 @@ package org.vending.machine;
 
 public class VendingMachineService {
 
-	
+    private static VendingMachineService instance;
+    private VendingMachineService() {
+    }
+
+    public static VendingMachineService getInstance(){
+        if(VendingMachineService.instance == null) {
+            VendingMachineService.instance = new VendingMachineService();
+        }
+        return VendingMachineService.instance;
+    }
+
+    private static double balanceAmount = 0.0d;
+
+    public boolean isCoinAcceptedDenomination(final String coin){
+        if (coin.equalsIgnoreCase("0.1") || coin.equalsIgnoreCase("0.2") || coin.equalsIgnoreCase("0.5")
+                || coin.equalsIgnoreCase("1") || coin.equalsIgnoreCase("2")){
+            return true;
+        }else{
+            return false;
+        }
+    }
 	
 }
--- src/test/java/org/vending/machine/VendingMachineApplicationTest.java
+++ src/test/java/org/vending/machine/VendingMachineApplicationTest.java
@@ -1,9 +1,27 @@
 package org.vending.machine;
 
 
-public class VendingMachineApplicationTest {
-	
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class VendingMachineApplicationTest {
+
+	VendingMachineService vendingMachineService;
+
+	@Before
+	public void setUp() {
+		vendingMachineService = VendingMachineService.getInstance();
+	}
+	@Test
+	public void canInsertValidCoin(){
+		Assert.assertTrue(vendingMachineService.isCoinAcceptedDenomination("1"));
+	}
+
+	@Test
+	public void canNotInsertInvalidCoin(){
+		Assert.assertFalse(vendingMachineService.isCoinAcceptedDenomination("0.05"));
+	}
 	
 
 }
